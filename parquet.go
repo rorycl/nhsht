04d016538bfafaebe9ffec449350bdc219aa69f5
rorycl <rorycl@campbell-lange.net>
2025-09-27 22:06:02 +0100

    parquet: rework API for better error catching, add reading

--- parquet.go
+++ parquet.go
@@ -2,48 +2,110 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io"
 	"os"
 
 	"github.com/parquet-go/parquet-go"
 )
 
 // NHSNoHash is a struct for encoding a pair of values representing an
-// NHS Number and a salted and SHA256 hash of the NHSNo for serialising
-// into parquet format.
+// NHS Number and a salted SHA256 hash of the NHSNo for serialising into
+// parquet format. For efficiency the NHSNo is stored as an int32 which
+// precludes storing NHS numbers with leading zeros.
 type NHSNoHash struct {
-	NHSNo int32  `parquet:"nhsno,zstd"`
+	NHSNo int32  `parquet:"nhsno,zstd"` // not a string
 	Hash  string `parquet:"hash,zstd"`
 }
 
-// parquetWriter writes NHSNoHash records to a parquet file by reading
-// from the readChan channel of NHSNoHash values.
-func parquetWriter(filename string, readChan <-chan NHSNoHash) error {
+// parquetWriter provides an NHSNoHash chan for writing records to a
+// parquet file, providing also an error chan for reporting writing or
+// other errors.
+//
+// Usage example:
+//
+//  // Initialiase parquetWriter.
+//	writerChan, errChan, err := parquetWriter(fn)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	// Write records to the parquet file.
+//	for i, rec := range testRecords {
+//		select {
+//		case writerChan <- rec: // check writer is still open
+//		case err := <-errChan: // fail if error
+//			// handle error
+//		}
+//	}
+//
+//	// Signal completion of writing of records to parquetWriter.
+//	close(writerChan)
+//
+//	// Synchronisation point to wait for flushing to disk.
+//	if err := <-errChan; err != nil {
+//		t.Fatal(err)
+//	}
+
+func parquetWriter(filename string) (chan<- NHSNoHash, <-chan error, error) {
+
 	var err error
+	writerChan := make(chan NHSNoHash)
+	errorChan := make(chan error, 1)
 
 	f, err := os.Create(filename)
 	if err != nil {
-		return fmt.Errorf("parquet file creation error: %w", err)
+		return nil, nil, fmt.Errorf("parquet file creation error: %w", err)
 	}
 
 	writer := parquet.NewWriter(f)
 
-	defer func() {
+	go func() {
+		defer close(errorChan)
+		i := 0
+		for record := range writerChan {
+			if err = writer.Write(record); err != nil {
+				errorChan <- fmt.Errorf("parquet file row %d write error: %w", i, err)
+				return
+			}
+			i++
+		}
+		// close parquet writer and flush file to disk
 		if err = writer.Close(); err != nil {
-			log.Printf("parquet file close error: %v", err)
+			errorChan <- fmt.Errorf("parquet writer close error: %w", err)
+			return
 		}
 		if err = f.Close(); err != nil {
-			log.Printf("file close error: %v", err)
+			errorChan <- fmt.Errorf("file close error: %w", err)
+			return
 		}
-		fmt.Println("done!")
 	}()
 
-	for record := range readChan {
-		if err = writer.Write(record); err != nil {
-			return err
-		}
-		fmt.Printf("wrote %#v\n", record)
+	return writerChan, errorChan, err
+}
+
+// parquetReader reads NHSNoHash records from a parquet file. This is
+// intended for testing purposes. To lookup an NHS number or hash using
+// duckdb is recommended.
+func parquetReader(filename string) ([]NHSNoHash, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
 	}
 
-	return err
+	pq := parquet.NewReader(f)
+	nhsHashes := []NHSNoHash{}
+	i := 0
+	for {
+		var nh NHSNoHash
+		err := pq.Read(&nh)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("parquet row %d reading error: %w", i, err)
+		}
+		nhsHashes = append(nhsHashes, nh)
+		i++
+	}
+	return nhsHashes, nil
 }
--- parquet_test.go
+++ parquet_test.go
@@ -3,42 +3,73 @@ package main
 import (
 	"fmt"
 	"os"
-	"sync"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
+
+var testRecords []NHSNoHash = []NHSNoHash{
+	NHSNoHash{300_000_000, "98ea6e4f216f2fb4b69fff9b3a44842c38686ca685f3f55dc48c5d3fb1107be4"},
+	NHSNoHash{300_000_001, "c641344867e9806fadfd219f25b62b97c94db0eed04a1d79e93676533cfb782b"},
+}
 
 func TestParquetWriter(t *testing.T) {
 
-	tmpFile, err := os.CreateTemp("", "tpw.parquet")
+	// Setup a test output parquet file.
+	tmpFile, err := os.CreateTemp("", "tpw_*.parquet")
 	if err != nil {
 		t.Fatal(err)
 	}
 	fn := tmpFile.Name()
-	// defer func() {
-	// 	_ = os.Remove(fn)
-	// }()
 
-	writeChan := make(chan NHSNoHash)
+	t.Cleanup(func() {
+		_ = os.Remove(fn) // remove the test file
+	})
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		err := parquetWriter(fn, writeChan)
-		if err != nil {
-			t.Fatal(err)
-		}
-		wg.Done()
-	}()
-
-	for i, rec := range []NHSNoHash{
-		NHSNoHash{300_000_000, "98ea6e4f216f2fb4b69fff9b3a44842c38686ca685f3f55dc48c5d3fb1107be4"},
-		NHSNoHash{300_000_001, "c641344867e9806fadfd219f25b62b97c94db0eed04a1d79e93676533cfb782b"},
-	} {
-		fmt.Printf("writing record %d: %v\n", i, rec)
-		writeChan <- rec
+	writerChan, errChan, err := parquetWriter(fn)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	close(writeChan)
-	wg.Wait()
+	// Write records to the parquet file.
+	for i, rec := range testRecords {
+		fmt.Printf("writing record %d: %v\n", i, rec)
+		select {
+		case writerChan <- rec: // check writer is still open
+		case err := <-errChan: // fail if error
+			t.Fatal(err)
+		}
+	}
+
+	// Signal completion of writing of records to parquetWriter.
+	close(writerChan)
+
+	// Synchronisation point to wait for parquet file to be closed and
+	// flushed to disk.
+	if err := <-errChan; err != nil {
+		t.Fatal(err)
+	}
+
+	// Check something was written to disk.
+	stat, err := os.Stat(fn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := stat.Size(), int64(500); got < want {
+		t.Errorf("got file size %d want at least %d", got, want)
+	}
+
+	// Check contents of parquet file.
+	nhs, err := parquetReader(fn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := len(nhs), len(testRecords); got != want {
+		t.Errorf("got %d records want %d", got, want)
+	}
+
+	if !cmp.Equal(nhs, testRecords) {
+		t.Error(cmp.Diff(nhs, testRecords))
+	}
 
 }

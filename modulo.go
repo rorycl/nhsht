38e953c8a5be87cf23c682b2da56f6511516e423
rorycl <rorycl@campbell-lange.net>
2025-09-28 10:03:46 +0100

    modulo: first implementation of the nhs modulo 11 algorithm

---
+++ modulo.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+var InvalidModulo11 error = errors.New("invalid nhs number")
+
+// modulo11 generates a modulo 11 addendum to a number based on the
+// specification of NHS numbers described at Wikipedia.
+//
+// https://en.wikipedia.org/wiki/NHS_number
+//
+// The checksum is calculated by multiplying each of the first nine
+// digits by 11 minus its position. Using the number 943 476 5919 as an
+// example:
+//   - The first digit is 9. This is multiplied by 10.
+//   - The second digit is 4. This is multiplied by 9.
+//   - And so on until the ninth digit (1) is multiplied by 2.
+//   - The result of this calculation is summed. In this example:
+//     (9×10) + (4×9) + (3×8) + (4×7) + (7×6) + (6×5) + (5×4) + (9×3) + (1×2) = 299.
+//   - The remainder when dividing this number by 11 is calculated,
+//     yielding a number in the range 0–10, which would be 2 in this case.
+//   - Finally, this number is subtracted from 11 to give the checksum in
+//     the range 1–11, in this case 9, which becomes the last digit of the
+//     NHS number.
+//   - A checksum of 11 is represented by 0 in the final NHS number. If
+//     the checksum is 10 then the number is not valid.
+func modulo11(number int) (int, error) {
+	weighter := func(num, pos int) int {
+		weight := 10 - pos
+		return num * weight
+	}
+	numberString := strconv.Itoa(number)
+	if len(numberString) != 9 {
+		return -1, fmt.Errorf("expected number 9 digits in length, got %d", len(numberString))
+	}
+	sum := 0
+	for i, digit := range numberString {
+		val, err := strconv.Atoi(string(digit))
+		if err != nil {
+			return -1, fmt.Errorf("could not convert %q to int", val)
+		}
+		sum += weighter(val, i)
+	}
+	remainder := 11 - (sum % 11)
+	switch remainder {
+	case 10:
+		return -1, InvalidModulo11
+	case 11:
+		remainder = 0
+	}
+	numberString += strconv.Itoa(remainder)
+	val, err := strconv.Atoi(numberString)
+	if err != nil {
+		return -1, fmt.Errorf("could not convert %q to int", val)
+	}
+	return val, nil
+}
---
+++ modulo_test.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestModulo11(t *testing.T) {
+
+	cases := []struct {
+		name   string
+		input  int
+		output int
+		isErr  bool
+	}{
+		{
+			name:   "too short",
+			input:  94347659,
+			output: -1,
+			isErr:  true,
+		},
+		{
+			name:   "too long",
+			input:  9434765911,
+			output: -1,
+			isErr:  true,
+		},
+		{
+			name:   "ok (remainder 11)",
+			input:  943476590,
+			output: 9434765900,
+			isErr:  false,
+		},
+		{
+			name:   "ok",
+			input:  943476591,
+			output: 9434765919,
+			isErr:  false,
+		},
+		{
+			name:   "ok",
+			input:  943476592,
+			output: 9434765927,
+			isErr:  false,
+		},
+		{
+			name:   "ok",
+			input:  943476593,
+			output: 9434765935,
+			isErr:  false,
+		},
+		{
+			name:   "ok",
+			input:  943476594,
+			output: 9434765943,
+			isErr:  false,
+		},
+		{
+			name:   "ok",
+			input:  943476595,
+			output: 9434765951,
+			isErr:  false,
+		},
+		{
+			name:   "invalid nhs number (checksum 10)",
+			input:  943476596,
+			output: -1,
+			isErr:  true,
+		},
+		{
+			name:   "ok",
+			input:  943476597,
+			output: 9434765978,
+			isErr:  false,
+		},
+		{
+			name:   "ok",
+			input:  943476598,
+			output: 9434765986,
+			isErr:  false,
+		},
+		{
+			name:   "ok",
+			input:  943476599,
+			output: 9434765994,
+			isErr:  false,
+		},
+	}
+
+	for ii, tt := range cases {
+		t.Run(fmt.Sprintf("%d_%s", ii, tt.name), func(t *testing.T) {
+			output, err := modulo11(tt.input)
+			if (err == nil) == tt.isErr {
+				t.Fatal(err)
+			}
+			if err != nil {
+				return
+			}
+			fmt.Printf("%d -> %d\n", tt.input, output)
+			if got, want := output, tt.output; got != want {
+				t.Errorf("got %d want %d", got, want)
+			}
+		})
+	}
+}

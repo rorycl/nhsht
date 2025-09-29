fd18b3d0ad3f2d36a3306023b0eac2fa5e882b10
rorycl <rorycl@campbell-lange.net>
2025-09-29 09:59:46 +0100

    generator: addition of orchestrator generator functions

---
+++ generator_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestGenerator(t *testing.T) {
+
+	numberOfRecords := 200_000
+	var err error
+
+	// Setup a test output salt and parquet file.
+	saltFile, err := os.CreateTemp("", "gensalt_*.sha256")
+	if err != nil {
+		t.Fatal(err)
+	}
+	sf := saltFile.Name()
+
+	parquetFile, err := os.CreateTemp("", "genparquet_*.parquet")
+	if err != nil {
+		t.Fatal(err)
+	}
+	pqf := parquetFile.Name()
+
+	t.Cleanup(func() {
+		if err != nil {
+			return // keep tmp files
+		}
+		_ = os.Remove(sf)
+		_ = os.Remove(pqf)
+	})
+
+	start := time.Now()
+	err = Generator(sf, pqf, numberOfRecords, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("generation time for %d records: %v\n",
+		numberOfRecords, time.Since(start),
+	)
+
+	// Check number of records in parquet file.
+	nhs, err := parquetReader(pqf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := len(nhs), numberOfRecords; got != want {
+		err = errors.New("invalid number of records")
+		t.Errorf("got %d records want %d", got, want)
+	}
+
+}

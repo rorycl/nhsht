b776378985a17db4ce7a985cae7d1f43375eefaf
rorycl <rorycl@campbell-lange.net>
2025-09-29 13:34:27 +0100

    main: adjust to changed flag parsing error reporting

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ func main() {
 
 	flags, err := ParseFlags()
 	if err != nil {
-		// jessevdk/go-flags emits its own error message
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	err = Generator(flags.SaltFile, flags.ParquetFile, int(flags.Records), flags.Verbose)
+	err = Generator(flags.SaltFile, flags.ParquetFile, int(flags.Records), int(flags.GoRoutines), flags.Verbose)
 	if err != nil {
 		fmt.Println("generator error:", err)
 		os.Exit(1)
